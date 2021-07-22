e7708b8a627a806c20ba096fdc66fd5bb9a9bcc7
ElectroSpyder <prg@live.com.ar>
2021-07-22 16:46:11 -0300

    Creacion de proyectos base

---
+++ TCT.Core/TCT.Core.Modelo/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TCT.Core.Modelo.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public int Tipo { get; set; }
+    }
+}
---
+++ TCT.Core/TCT.Core.Web/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿namespace TCT.Core.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
