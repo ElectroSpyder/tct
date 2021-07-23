ab329559a95b58448be86863e7b1742f90d1b320
ElectroSpyder <prg@live.com.ar>
2021-07-23 00:58:05 -0300

    Seleccion de un template y creacion de controlador de orden de trabajo

---
+++ TCT.Core/TCT.Core.Modelo/ViewModels/OrdenTrabajoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TCT.Core.Modelo.ViewModels
+{
+    public class OrdenTrabajoViewModel
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Inicio { get; set; }
+        public string Fin { get; set; }
+        public enum Estado {FINALIZADO, SUSPENDIDO, PROCESO, NO_INICIADO }
+    }
+}
---
+++ TCT.Core/TCT.Core.Web/Controllers/OrdenTrabajoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TCT.Core.Modelo.ViewModels;
+//https://bbbootstrap.com/snippets/request-demo-form-validation-12274818
+namespace TCT.Core.Web.Controllers
+{
+    public class OrdenTrabajoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new OrdenTrabajoViewModel();
+            return View(model);
+        }
+
+        public IActionResult AltaOT()
+        {
+            return View(new OrdenTrabajoViewModel());
+        }
+    }
+}
